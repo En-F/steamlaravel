a7e2c18d91c66ea7d76267790d05717dd1a58ff9
Enrique Franco <enrique.franco@iesdonana.org>
2025-11-13 19:31:17 +0100

    Mostrado de clientes  en la página y el método Borrar

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,4 +1,28 @@
 <x-app-layout>
-    Hola {{ $nombre }}
+    <div class="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default"></div>
+    <table class="w-full text-sm text-left rtl:text-right text-body">
+        <thead class="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
+            <th scope="col" class="px-6 py-3 font-medium">DNI</th>
+            <th scope="col" class="px-6 py-3 font-medium">Nombre</th>
+            <th scope="col" class="px-6 py-3 font-medium">Apellidos</th>
+            <th scope="col" class="px-6 py-3 font-medium">Acciones</th>
+        </thead>
+        <body>
+            @foreach ($clientes as $cliente)
+            <tr class="bg-neutral-primary border-b border-default">
+                <td class="px-6 py-4">{{ $cliente->dni }}</td>
+                <td class="px-6 py-4">{{ $cliente->nombre }}</td>
+                <td class="px-6 py-4">{{ $cliente->apellidos }}</td>
+                <td class="px-6 py-4">
+                    <form action="/clientes/borrar/{{ $cliente->id }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit">Borrar</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </body>
+    </table>
 </x-app-layout>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Cliente;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,11 +16,30 @@ Route::get('/hola', function () {
 
 Route::get('/clientes',function(){
     return view('clientes.index',[
-        'nombre' => 'Pepito',
+        'clientes' => Cliente::all(),
     ]);
 });
 
 
-// Route::get('/clientes/create',function(){
-//     return view('clientes.create');
-// });
+Route::get('/clientes/create',function(){
+    return view('clientes.create');
+    // Cliente::create([
+    //     'dni' => '3333',
+    //     'nombre' => 'Ronaldo',
+    //     'apellidos' => 'Franco',
+    //     'direccion' => 'Calle Ancha 89',
+    //     'codpostal'=> 19840,
+    //     'telefono' => '123456789'
+    // ]);
+});
+
+//marcador {} representa un valor que luego se va a recoger
+Route::delete('/clientes/borrar/{cliente}',function(Cliente  $cliente){
+    $cliente->delete();
+
+    //redireccion a otra página con un respons, pero se modifica para que nos deje en otro lado
+    return redirect('/clientes');
+});
+
+
+
