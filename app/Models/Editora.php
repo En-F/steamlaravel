5c293de0241d42cf46bff451237f1f33c5e4ca21
Enrique Franco <enrique.franco@iesdonana.org>
2025-11-24 21:27:03 +0100

    Modificar  los campos de las tablas desarrolladora y editora

--- app/Models/Editora.php
+++ app/Models/Editora.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Editora extends Model
 {
     protected $fillable = [
         'nombre'
     ];
+    public function desarrolladoras(): HasMany{
+        return $this->hasMany(Desarrolladora::class);
+    }
 }
